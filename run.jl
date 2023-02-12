fe970783196e2c578cf556cc59425e125af85891
iagorrr04 <iagorrr04@gmail.com>
2023-02-11 21:49:46 -0300

    :hammer: receives domain and instance as paramter

--- run.jl
+++ run.jl
@@ -1,12 +1,36 @@
-using PDDL, SymbolicPlanners
+using PDDL, SymbolicPlanners, ArgParse
 
-domain = load_domain("./PDDL-Bus/domain.pddl")
-problem = load_problem("./PDDL-Bus/instance.pddl")
-goal = PDDL.get_goal(problem)
+function parse_commandline()
+    s  = ArgParseSettings()
+    @add_arg_table s begin
+        "--domain"
+            arg_type = String
+            default = "" 
+        "--instance"
+            arg_type = String
+            default = "" 
+    end
+    return parse_args(s)
+end
 
-c_domain, c_problem= compiled(domain, problem)
-spec = MinStepsGoal(PDDL.get_goal(problem))
-planner = AStarPlanner(HAdd())
+function run_solver(domain_path, instance_path)
+    domain = load_domain(domain_path)
+    problem = load_problem(instance_path)
+    state = initstate(domain, problem)
+    goal = PDDL.get_goal(problem)
+    spec = MinStepsGoal(problem)
+    planner = ForwardPlanner(heuristic=HAdd())
+    sol = planner(domain, state, spec)
+    print(sol)
+end
 
-sol = planner(c_domain, c_problem, spec)
-print(sol)
+function main()
+    @show parsed_args = parse_commandline()
+
+    domain_path = parsed_args["domain"]
+    instance_path = parsed_args["instance"]
+
+    @show run_solver(domain_path, instance_path) 
+end
+
+main()
